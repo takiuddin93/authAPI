d11b3b7e78affea4c95c2e589cc3e9f063a98251
Taki Uddin <takiuddin93@gmail.com>
2020-04-26 02:32:40 +0600

    created configs folder with database connection variables

---
+++ configs/database.js
@@ -0,0 +1,33 @@
+const mysql = require('mysql2')
+const pool = mysql.createPool({
+  host: '192.168.121.193',
+  user: 'root',
+  password: 'password',
+  database: 'asiatic360',
+  connectionLimit: 8,
+  port: '4000'
+})
+
+// Ping database to check for common exception errors.
+pool.getConnection((err, connection) => {
+  if (err) {
+    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+      console.error('Database connection was closed.')
+    }
+    if (err.code === 'ER_CON_COUNT_ERROR') {
+      console.error('Database has too many connections.')
+    }
+    if (err.code === 'ECONNREFUSED') {
+      console.error('Database connection was refused.')
+    }
+  }
+
+  if (connection) connection.release()
+
+  return
+})
+
+// Promisify for Node.js async/await.
+pool.query = util.promisify(pool.query)
+
+module.exports = pool
