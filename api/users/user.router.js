db462fefbf41caaa4a809bfc2d7eb6e4b842a1cc
Taki Uddin <takiuddin93@gmail.com>
2020-04-26 17:25:17 +0600

    login integration

--- api/users/user.router.js
+++ api/users/user.router.js
@@ -2,7 +2,8 @@ const router = require("express").Router();
 const {
   create_Users,
   get_Users,
-  search_By_e_id
+  search_By_e_id,
+  login
 } = require("./user.controller");
 router.post("/", create_Users);
 router.get("/", get_Users);
